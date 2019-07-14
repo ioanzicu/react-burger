7d09b217d74d883da43839cb7c74314f1ae4146a
Ioan Zicu <ioan.zicu94@gmail.com>
2019-07-14 13:36:37 +0200

    Burger Component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 
 import Auxiliar from "../../hoc/Auxiliar";
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
   state = {};
   render() {
     return (
       <Auxiliar>
-        <div>Burger</div>
+        <Burger />
         <div>Build Control</div>
       </Auxiliar>
     );
