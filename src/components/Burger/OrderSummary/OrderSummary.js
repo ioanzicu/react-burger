7a633cafc2572afb173498461436c1ca9891d78e
Ioan Zicu <ioan.zicu94@gmail.com>
2019-07-14 21:28:05 +0200

    Order Summary Modal

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import Auxiliar from "../../../hoc/Auxiliar";
+
+const orderSummary = props => {
+  const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
+    return (
+      <li>
+        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+        {props.ingredients[igKey]}
+      </li>
+    );
+  });
+
+  return (
+    <Auxiliar>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following infredients:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>Continue to Checkout?</p>
+    </Auxiliar>
+  );
+};
+
+export default orderSummary;
