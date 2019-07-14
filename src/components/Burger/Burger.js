4ad6954c6d993c5155793bfb36b404058ae75d06
Ioan Zicu <ioan.zicu94@gmail.com>
2019-07-14 15:19:38 +0200

    Conditional Rendering of Ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,17 +3,24 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    // ['salad', 'bacon', 'cheese', 'meat']
-    console.log([...Array(props.ingredients[igKey])]);
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      // Return an array with a N - number of objects
-      // Array(2) - create an array with length of 2
-      // .map() over that array N times
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
-  console.log(transformedIngredients);
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      // ['salad', 'bacon', 'cheese', 'meat']
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        // Return an array with a N - number of objects
+        // Array(2) - create an array with length of 2
+        // .map() over that array N times
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
