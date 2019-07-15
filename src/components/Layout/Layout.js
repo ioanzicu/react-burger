1928c93c59645a18e98df643ae89a60f7ec25fc8
Ioan Zicu <ioan.zicu94@gmail.com>
2019-07-15 20:32:07 +0200

    Responsive SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,12 @@ import React from "react";
 import Auxiliar from "../../hoc/Auxiliar";
 import classes from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = props => (
   <Auxiliar>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </Auxiliar>
 );
