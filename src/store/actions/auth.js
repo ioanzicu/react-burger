c369a469d3ce4c9f8996e4ccb7d402b3ad5daf23
Ioan Zicu <ioan.zicu94@gmail.com>
2019-08-04 22:31:22 +0200

    Persist Auth State with localStorage

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -17,6 +17,9 @@ export const authSuccess = (token, userId) => {
 }
 
 export const authFail = error => {
+  localStorage.removeItem('token')
+  localStorage.removeItem('expirationDate')
+  localStorage.removeItem('userId')
   return {
     type: actionTypes.AUTH_FAIL,
     error: error
@@ -55,11 +58,18 @@ export const auth = (email, password, isSignup) => {
       .post(url, authData)
       .then(response => {
         console.log(response)
+        // SET TOKEN ON LOCAL  with build in JS -> localStorage
+        const expirationDate = new Date(
+          new Date().getTime() + response.data.expiresIn * 1000
+        )
+        localStorage.setItem('token', response.data.idToken)
+        localStorage.setItem('expirationDate', expirationDate)
+        localStorage.setItem('userId', response.data.localId)
         dispatch(authSuccess(response.data, response.data.localId))
         dispatch(checkAuthTimeout(response.data.expiresIn))
       })
       .catch(err => {
-        dispatch(authFail(err.response.data.error))
+        dispatch(authFail(err))
       })
   }
 }
@@ -68,5 +78,27 @@ export const setAuthRedirectPath = path => {
   return {
     type: actionTypes.SET_AUTH_REDIRECT_PATH,
     path: path
+  }
+}
+
+export const authCheckState = () => {
+  return dispatch => {
+    const token = localStorage.getItem('token')
+    if (!token) {
+      dispatch(logout())
+    } else {
+      const expirationDate = new Date(localStorage.getItem('expirationDate'))
+      if (expirationDate > new Date()) {
+        dispatch(logout())
+      } else {
+        const userId = localStorage.getItem('userId')
+        dispatch(authSuccess(token, userId))
+        dispatch(
+          checkAuthTimeout(
+            expirationDate.getSeconds() - new Date().getSeconds()
+          )
+        )
+      }
+    }
   }
 }
