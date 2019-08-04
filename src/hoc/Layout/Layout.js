6958781416ec49394f2f3182a3ad5b0703541338
Ioan Zicu <ioan.zicu94@gmail.com>
2019-08-04 19:49:20 +0200

    Update UI depending on Auth State

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,37 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import Auxiliar from '../Auxiliar/Auxiliar';
-import classes from './Layout.css';
-import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
-import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+import { connect } from 'react-redux'
+
+import Auxiliar from '../Auxiliar/Auxiliar'
+import classes from './Layout.css'
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
 
 class Layout extends Component {
   state = {
     showSideDrawer: false
-  };
+  }
 
   sideDrawerClosedHandler = () => {
-    this.setState({ showSideDrawer: false });
-  };
+    this.setState({ showSideDrawer: false })
+  }
 
   sideDrawerToggleHandler = () => {
     this.setState(prevState => {
-      return { showSideDrawer: !this.state.showSideDrawer };
-    });
-  };
+      return { showSideDrawer: !this.state.showSideDrawer }
+    })
+  }
 
   render() {
     return (
       <Auxiliar>
-        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <Toolbar
+          isAuth={this.props.isAuthenticated}
+          drawerToggleClicked={this.sideDrawerToggleHandler}
+        />
         <SideDrawer
+          isAuth={this.props.isAuthenticated}
           open={this.state.showSideDrawer}
           closed={this.sideDrawerClosedHandler}
         />
         <main className={classes.Content}>{this.props.children}</main>
       </Auxiliar>
-    );
+    )
   }
 }
 
-export default Layout;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  }
+}
+
+export default connect(mapStateToProps)(Layout)
