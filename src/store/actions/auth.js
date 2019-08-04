18234d3b94c242a448fda0d8340d61db933d39f2
Ioan Zicu <ioan.zicu94@gmail.com>
2019-08-04 13:31:30 +0200

    Add Sign-in

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,7 +22,7 @@ export const authFail = error => {
   }
 }
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
   return dispatch => {
     dispatch(authStart())
     const authData = {
@@ -30,11 +30,14 @@ export const auth = (email, password) => {
       password: password,
       returnSecureToken: true
     }
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API-KEY]'
+    if (!isSignup) {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API-KEY]'
+    }
     axios
-      .post(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBxiR21xFKgmOSrX5q2xcWWa48Im2FQeKw',
-        authData
-      )
+      .post(url, authData)
       .then(response => {
         console.log(response)
         dispatch(authSuccess(response.data))
