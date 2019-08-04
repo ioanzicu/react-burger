e1e8a3bba0ed88cf0174a3f5939d0cc7e2a6b618
Ioan Zicu <ioan.zicu94@gmail.com>
2019-08-04 20:55:06 +0200

    Redirect the User to the Checkout Page

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,42 +1,45 @@
-import * as actionTypes from '../actions/actionTypes';
-import { updateObject } from '../utility';
+import * as actionTypes from '../actions/actionTypes'
+import { updateObject } from '../utility'
 
 const initialState = {
   ingredients: null,
   totalPrice: 4,
-  error: false
-};
+  error: false,
+  building: false
+}
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
   cheese: 0.4,
   meat: 1.3,
   bacon: 0.7
-};
+}
 
 const addIngredient = (state, action) => {
   const updatedIngredient = {
     [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-  };
-  const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+  }
+  const updatedIngredients = updateObject(state.ingredients, updatedIngredient)
   const updatedState = {
     ingredients: updatedIngredients,
-    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-  };
-  return updateObject(state, updatedState);
-};
+    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+    building: true
+  }
+  return updateObject(state, updatedState)
+}
 
 const removeIngredient = (state, action) => {
   const updatedIng = {
     [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-  };
-  const updatedIngs = updateObject(state.ingredients, updatedIng);
+  }
+  const updatedIngs = updateObject(state.ingredients, updatedIng)
   const updatedSt = {
     ingredients: updatedIngs,
-    totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
-  };
-  return updateObject(state, updatedSt);
-};
+    totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
+    building: true
+  }
+  return updateObject(state, updatedSt)
+}
 
 const setIngredients = (state, action) => {
   return updateObject(state, {
@@ -47,27 +50,28 @@ const setIngredients = (state, action) => {
       meat: action.ingredients.meat
     },
     totalPrice: 4,
-    error: false
-  });
-};
+    error: false,
+    building: true
+  })
+}
 
 const fetchIngredientsFailed = (state, action) => {
-  return updateObject(state, { error: true });
-};
+  return updateObject(state, { error: true })
+}
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_INGREDIENT:
-      return addIngredient(state, action);
+      return addIngredient(state, action)
     case actionTypes.REMOVE_INGREDIENT:
-      return removeIngredient(state, action);
+      return removeIngredient(state, action)
     case actionTypes.SET_INGREDIENTS:
-      return setIngredients(state, action);
+      return setIngredients(state, action)
     case actionTypes.FETCH_INGREDIENTS_FAILED:
-      return fetchIngredientsFailed(state, action);
+      return fetchIngredientsFailed(state, action)
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default reducer;
+export default reducer
