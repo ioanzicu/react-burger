7190875fef026fe627359df8cb60a89dd314272d
Ioan Zicu <ioan.zicu94@gmail.com>
2019-08-24 13:59:25 +0200

    Add Hooks to ContactData

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -24,9 +24,10 @@ export const purchaseBurgerStart = () => {
 
 export const purchaseBurger = (orderData, token) => {
   return dispatch => {
+    console.log('token', token)
     dispatch(purchaseBurgerStart())
     axios
-      .post(`/orders.json?auth=${token ? token.idToken : ''}`, orderData)
+      .post(`/orders.json?auth=${token}`, orderData)
       .then(response => {
         dispatch(purchaseBurgerSuccess(response.data.name, orderData))
       })
